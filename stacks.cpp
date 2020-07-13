ee5e48fd6991e13e74f32fa445e4858911b665e4
Yash Tyagi <65150966+yash-tyagi99@users.noreply.github.com>
2020-07-13 11:10:17 +0530

    Update stacks.cpp

--- stacks.cpp
+++ stacks.cpp
@@ -9,6 +9,7 @@ class Stack{
 	 int arr[5];
 
 	public:
+	//Intially stack is null..
 	 Stack()
 	 {
 		top=-1;
@@ -17,6 +18,7 @@ class Stack{
 		arr[i]=0;
            }
 	}
+	//Check the stack is empty or not?
 
 	bool isEmpty()
 	{
@@ -25,16 +27,20 @@ class Stack{
 		else
 		 return false;
 	}
-	bool isFull()
+	//Check the stack is full or not?
+	
+	bool isFull() // bool is a data type with two possible values: true or false..
 	{
 		if(top==4)
 		 return true;
 		else
 		 return false;
 	}
+	//For insert the value in the stack..
+	
 	void push(int val)
 	{
-		if(isFull())
+		if(isFull()) //check the stack is full..
 		{
 		 cout<<"stack overflow"<<endl;
 		}
@@ -44,9 +50,11 @@ class Stack{
 		 arr[top]=val;
 		}
 	}
+	//For delete the value in the stack..
+	
 	int pop()
 	{
-		if(isEmpty())
+		if(isEmpty()) //check the stack is empty..
 		{
 		 cout<<"stack underflow"<<endl;
 		 return 0;
@@ -59,10 +67,14 @@ class Stack{
 		 return popValue;
 		}
 	}
+	//For count(get the number of items in the stack) the value in the stack..
+	
 	int count()
 	{
 		return(top+1);
 	}
+	// For peek(access the item at the i position) the value in the stack..
+	
 	int peek(int pos)
 	{
 		if(isEmpty())
@@ -75,11 +87,15 @@ class Stack{
 		 return arr[pos];
 		}
 	}
+	//For change(change the item at the i position) the value in the stack..
+)
 	void change(int pos, int val)
 	{
 	 arr[pos]=val;
 	 cout<<"value changed at location" <<pos<<endl;
 	}
+	//For display(display all items in the stack) the value in the stack..
+	
 	void display()
  	{
 		cout<<"All value are in stack"<<endl;
@@ -97,6 +113,7 @@ int main()
 {
 	Stack s1;
 	int option,position,value;
+	//Run all the function..
 	do
 	{
 		cout<<"What opertion do you want to perform? Select Option number.Enter 0 to exit."<<endl;
@@ -115,33 +132,33 @@ int main()
 		{
 			case 0:
 			  break;
-			case 1:
+			case 1: //For push operation
 			 cout<<"Enter an item to push in the stack"<<endl;
 			 cin>>value;
 			 s1.push(value);
 			 break;
-			case 2:
-			 cout<<"Pop function  Called - Poped value: "<<s1.pop()<<endl;
+			case 2: //For pop operation
+			 cout<<"Pop function  Called - Poped value: "<<s1.pop()<<endl; 
 			 break;
-			case 3:
+			case 3: //for isEmpty operation
 			 if(s1.isEmpty())
-			  cout<<"stack is empty"<<endl;
+			  cout<<"stack is empty"<<endl;  
 			 else
 			  cout<<"stack is not empty"<<endl;
-			case 4:
+			case 4: //For isFull opertion
 			 if(s1.isEmpty())
 			  cout<<"stack is full"<<endl;
 			 else
 			  cout<<"stack is not full"<<endl;
-			case 5:
+			case 5://For peek opertion
 			  cout<<"Enter position of item you want to peek: "<<endl;
 			  cin>>position;
 			  cout<<"Peek Function Called - Value at Position "<<position<<" is "<<s1.peek(position)<<endl;
 			  break;
-			case 6:
+			case 6: //For count opertion
 			  cout<<"Count Function Called - Number of Items in the stack are: "<<s1.count()<<endl;
 			  break;
-			case 7:
+			case 7: //For change opertion
 			  cout<<"change function called - "<<endl;
 			  cout<<"Enter position of item you want to change : ";
 			  cin>>position;
@@ -150,11 +167,11 @@ int main()
 			  cin>>value;
 			  s1.change(position,value);
 			  break;
-			case 8:
+			case 8: // for display function
 			  cout<<"Display Function  Called -"<<endl;
 			  s1.display();
 			  break;
-			case 9:
+			case 9: //For clear screen
 			  system("cls");
 			  break;
 			default:
